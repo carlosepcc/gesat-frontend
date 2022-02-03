1110dcf765d858c8b94eb4cbe31088f3e4158a2c
Carlos Enrique <carlosepcardenas@gmail.com>
2022-02-03 00:29:53 -0500

    Error message notification.
    Axios base url

--- src/pages/lists/Users.vue
+++ src/pages/lists/Users.vue
@@ -10,22 +10,15 @@
 <script setup>
 import { ref } from "vue";
 import ListPage from 'components/ListPage'
-import axios from 'axios'
-import global from 'src/services/global'
+import { api } from 'boot/axios'
+import {useQuasar} from 'quasar'
+const $q = useQuasar()
 
-const { state, addArtefacto } = global
 const users = ref([])
 const userFields = ref([])
 
 userFields.value = [
   {
-    name: 'id',
-    required: true,
-    label: 'ID',
-    align: 'left',
-    field: 'id',
-    sortable: true,
-  },{
     name: 'username',
     required: true,
     label: 'Nombre de usuario',
@@ -33,92 +26,15 @@ userFields.value = [
     field: 'username',
     sortable: true,
   },{
-    name: 'rol',
+    name: 'roles',
     required: true,
-    label: 'Rol',
+    label: 'Roles',
     align: 'center',
-    field: 'rol',
+    field: 'roles',
     sortable: true,
-  },{
-    name: 'email',
-    required: true,
-    label: 'email',
-    align: 'left',
-    field: 'email',
-    sortable: true,
-  },{
-    name: 'first_name',
-    required: true,
-    label: 'Nombre',
-    align: 'left',
-    field: 'first_name',
-    sortable: true,
-  },{
-    name: 'last_name',
-    required: true,
-    label: 'Apellidos',
-    align: 'left',
-    field: 'last_name',
-    sortable: true,
-  },{
-    name: 'avatar',
-    required: true,
-    label: 'Foto',
-    align: 'left',
-    field: 'avatar',
-    sortable: false,
-  },
+  }
 ]
-users.value = [
-{
-"id": 7,
-"rol":"Asesor de calidad",
-"email": "michael.lawson@reqres.in",
-"first_name": "Michael",
-"last_name": "Lawson",
-"avatar": "https://reqres.in/img/faces/7-image.jpg"
-},
-{
-"id": 8,
-"rol":"Revisor",
-"email": "lindsay.ferguson@reqres.in",
-"first_name": "Lindsay",
-"last_name": "Ferguson",
-"avatar": "https://reqres.in/img/faces/8-image.jpg"
-},
-{
-"id": 9,
-"rol":"Coordinador de calidad",
-"email": "tobias.funke@reqres.in",
-"first_name": "Tobias",
-"last_name": "Funke",
-"avatar": "https://reqres.in/img/faces/9-image.jpg"
-},
-{
-"id": 10,
-"rol":"Encargado de proyecto",
-"email": "byron.fields@reqres.in",
-"first_name": "Byron",
-"last_name": "Fields",
-"avatar": "https://reqres.in/img/faces/10-image.jpg"
-},
-{
-"id": 11,
-"rol":"Encargado de proyecto",
-"email": "george.edwards@reqres.in",
-"first_name": "George",
-"last_name": "Edwards",
-"avatar": "https://reqres.in/img/faces/11-image.jpg"
-},
-{
-"id": 12,
-"rol":"Revisor",
-"email": "rachel.howell@reqres.in",
-"first_name": "Rachel",
-"last_name": "Howell",
-"avatar": "https://reqres.in/img/faces/12-image.jpg"
-}
-]/* 
+/* 
 function removeRow(id = 1, rows) {
 console.log('function remove triggered')
   var index = rows.findIndex(function(currentValue){
@@ -135,15 +51,21 @@ if (index != -1){
 }*/
 
 function listarUsuarios() {
-axios.get('https://mdc-gesat.herokuapp.com/usuario')
+api.get('/usuario')
   .then(function (response) {
     // handle success
     users.value = response.data
-    console.log(response);
+    console.log(users);
   })
   .catch(function (error) {
     // handle error
     console.log(error);
+    $q.notify({
+          color: 'negative',
+          position: 'top',
+          message: `${error.message}. Carga fallida. Revise su conexión a internet`,
+          icon: 'report_problem'
+        })
   })
   .then(function () {
     // always executed
